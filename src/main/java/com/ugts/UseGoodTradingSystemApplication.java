7e2ab80fa5a0b4f366b4c6f9dc0ef913081c40d9
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-05-06 21:33:48 +0700

    build: initialize base of BE server

---
+++ src/main/java/com/ugts/UseGoodTradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ugts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseGoodTradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseGoodTradingSystemApplication.class, args);
+	}
+
+}
