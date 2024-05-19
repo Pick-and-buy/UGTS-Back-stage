235597da980d5fd7555cea994bc90db786c2fddc
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-05-19 15:00:39 +0700

    test: add unit test for brand controller happy case

--- src/test/java/com/ugts/brand/controller/BrandControllerTest.java
+++ src/test/java/com/ugts/brand/controller/BrandControllerTest.java
@@ -1,9 +1,9 @@
 package com.ugts.brand.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -26,6 +26,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource("/test.properties")
@@ -46,7 +49,7 @@ class BrandControllerTest {
     }
 
     @Test
-    void testCreateBrand() throws Exception {
+    void testCreateBrand_success() throws Exception {
         // given
         BrandRequest request = new BrandRequest();
         request.setName("Test Brand");
@@ -76,5 +79,112 @@ class BrandControllerTest {
 
     private String asJsonString(Object obj) throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(obj);
+    }
+
+    @Test
+    void testGetAllBrands_success() throws Exception {
+        // given
+        BrandResponse brandResponse1 = new BrandResponse();
+        brandResponse1.setId(1L);
+        brandResponse1.setName("Brand 1");
+
+        BrandResponse brandResponse2 = new BrandResponse();
+        brandResponse2.setId(2L);
+        brandResponse2.setName("Brand 2");
+
+        List<BrandResponse> responses = Arrays.asList(brandResponse1, brandResponse2);
+
+        when(brandService.getAllBrands()).thenReturn(responses);
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/brands"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // then
+        verify(brandService).getAllBrands();
+
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        ApiResponse<List<BrandResponse>> apiResponse = objectMapper.readValue(responseBody, new TypeReference<>() {
+        });
+        assertNotNull(apiResponse.getResult());
+        assertEquals(2, apiResponse.getResult().size());
+        assertEquals(brandResponse1.getId(), apiResponse.getResult().get(0).getId());
+        assertEquals(brandResponse1.getName(), apiResponse.getResult().get(0).getName());
+        assertEquals(brandResponse2.getId(), apiResponse.getResult().get(1).getId());
+        assertEquals(brandResponse2.getName(), apiResponse.getResult().get(1).getName());
+    }
+
+    @Test
+    void testGetBrandByName_success() throws Exception {
+        // given
+        String name = "Test Brand";
+
+        BrandResponse response = new BrandResponse();
+        response.setId(1L);
+        response.setName(name);
+
+        when(brandService.getBrandByName(name)).thenReturn(response);
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/brands/{name}", name))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // then
+        verify(brandService).getBrandByName(name);
+
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        ApiResponse<BrandResponse> apiResponse = objectMapper.readValue(responseBody, new TypeReference<>() {
+        });
+        assertNotNull(apiResponse.getResult());
+        assertEquals(response.getId(), apiResponse.getResult().getId());
+        assertEquals(response.getName(), apiResponse.getResult().getName());
+    }
+
+    @Test
+    void testDeleteBrand_success() throws Exception {
+        // given
+        String name = "Test Brand";
+
+        // when
+        mockMvc.perform(delete("/api/v1/brands/{name}", name))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // then
+        verify(brandService).deleteBrand(name);
+    }
+
+    @Test
+    void testUpdateBrand_success() throws Exception {
+        // given
+        String name = "Test Brand";
+        BrandRequest request = new BrandRequest();
+        request.setName("Updated Brand");
+
+        BrandResponse response = new BrandResponse();
+        response.setId(1L);
+        response.setName(request.getName());
+
+        when(brandService.updateBrand(name, request)).thenReturn(response);
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(put("/api/v1/brands/{name}", name)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(request)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // then
+        verify(brandService).updateBrand(name, request);
+
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        ApiResponse<BrandResponse> apiResponse = objectMapper.readValue(responseBody, new TypeReference<>() {
+        });
+        assertNotNull(apiResponse.getResult());
+        assertEquals(response.getId(), apiResponse.getResult().getId());
+        assertEquals(response.getName(), apiResponse.getResult().getName());
+        assertEquals("Update Success", apiResponse.getMessage());
     }
 }
