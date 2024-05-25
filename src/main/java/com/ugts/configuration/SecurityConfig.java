c9c223915d442c85bd971a3a2d18cf0a3f645fe0
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-05-25 19:09:03 +0700

    build(auth): add OAuth2 Client dependency and config properties

--- src/main/java/com/ugts/configuration/SecurityConfig.java
+++ src/main/java/com/ugts/configuration/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINT)
+        http.authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINT)
                         .permitAll()
                         .anyRequest()
                         .authenticated())
