8d5a7695500caec44d13f91bc557b16b21d5db9b
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-06-03 01:12:06 +0700

    feat(auth): refactor code

--- src/main/java/com/ugts/configuration/GoogleCloudStorageConfig.java
+++ src/main/java/com/ugts/configuration/GoogleCloudStorageConfig.java
@@ -1,9 +1,7 @@
 package com.ugts.configuration;
 
 import java.io.IOException;
-import java.io.InputStream;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +12,6 @@ public class GoogleCloudStorageConfig {
 
     @Bean
     public Storage storage() throws IOException {
-        // Load the credentials file from the classpath
-        InputStream credentialsStream = getClass().getResourceAsStream("/google-credentials.json");
-        if (credentialsStream == null) {
-            throw new IOException("Failed to load credentials file");
-        }
-        GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
-        return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        return StorageOptions.getDefaultInstance().getService();
     }
 }
