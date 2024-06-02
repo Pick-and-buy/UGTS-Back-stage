6f194c3b98732598a7cc5f587ac6a0e976e36bd6
thanhlam1211 <lambthe150099@fpt.edu.vn>
2024-06-02 17:46:58 +0700

    chore: fixing chore naming

---
+++ src/test/java/com/ugts/comment/controller/CommentControllerTest.java
@@ -0,0 +1,16 @@
+package com.ugts.comment.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentControllerTest {
+
+    @Test
+    void createComments() {
+    }
+
+    @Test
+    void getCommentsByPostId() {
+    }
+}
