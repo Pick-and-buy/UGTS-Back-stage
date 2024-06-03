409af56cbb1bfda7e9ffc26afbedd593f6fd19bc
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-06-03 23:30:25 +0700

    feat(post): add get all post feature

--- src/main/java/com/ugts/post/dto/response/PostResponse.java
+++ src/main/java/com/ugts/post/dto/response/PostResponse.java
@@ -9,6 +9,7 @@ import lombok.Data;
 @Data
 @Builder
 public class PostResponse {
+    private String id;
     private String title;
     private String description;
     Boolean isAvailable;
--- src/main/java/com/ugts/post/service/PostService.java
+++ src/main/java/com/ugts/post/service/PostService.java
@@ -1,11 +1,15 @@
 package com.ugts.post.service;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.ugts.post.dto.request.CreatePostRequest;
 import com.ugts.post.dto.response.PostResponse;
+import com.ugts.post.entity.Post;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface PostService {
     PostResponse createPost(CreatePostRequest postRequest, MultipartFile[] files) throws IOException;
+
+    List<PostResponse> getAllPosts();
 }
