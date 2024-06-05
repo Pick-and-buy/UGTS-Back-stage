8a510bfd042ffc37211dba35fbe5212dccbf5ff9
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-06-06 01:52:00 +0700

    fix(auth): add public endpoint

--- src/main/java/com/ugts/configuration/SecurityConfig.java
+++ src/main/java/com/ugts/configuration/SecurityConfig.java
@@ -21,7 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private static final String[] PUBLIC_ENDPOINT = {
-        "/api/v1/auth/**", "/api/v1/otp/**",
+        "/api/v1/auth/**", "/api/v1/otp/**"
     };
 
     private final CustomJwtDecoder customJwtDecoder;
@@ -29,6 +29,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINT)
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/v1/posts/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
