993bbdb2a7fc815b5099af4c981706202905614a
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-06-19 04:35:30 +0700

    feat(register): handle CCU for register function

--- src/main/java/com/ugts/exception/ErrorCode.java
+++ src/main/java/com/ugts/exception/ErrorCode.java
@@ -31,6 +31,7 @@ public enum ErrorCode {
             1005,
             "Password must contain at least 1 small letter, capital letter, number and special character",
             HttpStatus.BAD_REQUEST),
+    USERNAME_EXISTED(1006, "Username has already existed", HttpStatus.BAD_REQUEST),
 
     INVALID_DOB(1006, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
     PHONE_NUMBER_INVALID(1007, "Your phone number must be {min} number", HttpStatus.BAD_REQUEST),
