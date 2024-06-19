ea93c382f7948eafd0aadd2921a756c31a0798bd
thanhlam1211 <lambthe150099@fpt.edu.vn>
2024-06-19 17:17:31 +0700

    update: adding comment time in repsonse and in entity

--- src/main/java/com/ugts/comment/util/BadWordConfig.java
+++ src/main/java/com/ugts/comment/util/BadWordConfig.java
@@ -19,6 +19,8 @@ public class BadWordConfig {
 
     private List<String> badWords = Arrays.asList(
             "địt",
+            "du ma",
+            "duma",
             "parky",
             "paky",
             "namki",
