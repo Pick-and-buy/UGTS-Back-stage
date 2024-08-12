2097f2fbe60fd99e3fcad3732045118e0700c586
thanhlam1211 <lambthe150099@fpt.edu.vn>
2024-08-12 19:09:13 +0700

    update: adding auto complete data to store in db

---
+++ src/main/java/com/ugts/product/entity/ProductData.java
@@ -0,0 +1,43 @@
+package com.ugts.product.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer productDataId;
+
+    String productName;
+
+    String brandLine;
+
+    String brand;
+
+    String category;
+
+    String color;
+
+    String size;
+
+    String width;
+
+    String height;
+
+    String length;
+
+    String exteriorMaterial;
+
+    String interiorMaterial;
+
+    String story;
+
+}
